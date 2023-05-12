a36e9340686376823018d670fdc1ac8a73c6780a
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2023-05-12 07:30:37 +0100

    First commit

---
+++ ai_models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# (C) Copyright 2023 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# (C) Copyright 2023 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+
+import io
+import os
+
+import setuptools
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return io.open(file_path, encoding="utf-8").read()
+
+
+version = None
+for line in read("ai_models/__init__.py").split("\n"):
+    if line.startswith("__version__"):
+        version = line.split("=")[-1].strip()[1:-1]
+
+
+assert version
+
+
+setuptools.setup(
+    name="ai-models",
+    version=version,
+    description="A run ai weather models",
+    long_description=read("README.md"),
+    author="European Centre for Medium-Range Weather Forecasts (ECMWF)",
+    author_email="software.support@ecmwf.int",
+    license="Apache License Version 2.0",
+    url="https://github.com/ecmwf/ai-models",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    zip_safe=True,
+    keywords="tool",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+    ],
+)
