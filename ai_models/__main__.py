7800a86a51abc129309c305a95194995c91caae5
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2023-05-12 07:32:34 +0100

    update

---
+++ ai_models/__main__.py
@@ -0,0 +1,166 @@
+# (C) Copyright 2023 European Centre for Medium-Range Weather Forecasts.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+import argparse
+import logging
+import os
+import shlex
+import subprocess
+import sys
+
+from .model import available_models, load_model
+
+LOG = logging.getLogger(__name__)
+
+
+def check_gpus():
+    try:
+        n = 0
+        for line in subprocess.check_output(["nvidia-smi", "-L"], text=True).split(
+            "\n"
+        ):
+            if line.startswith("GPU"):
+                n += 1
+        return n
+    except subprocess.CalledProcessError:
+        return 0
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--models",
+        action="store_true",
+        help="List models and exit",
+    )
+
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Turn on debug",
+    )
+
+    parser.add_argument(
+        "--input",
+        default="mars",
+        help="Source to use",
+    )
+
+    parser.add_argument(
+        "--file",
+        help="Source to use if source=file",
+    )
+
+    parser.add_argument(
+        "--output",
+        default="file",
+        help="Source to use",
+    )
+
+    parser.add_argument(
+        "--date",
+        default="-1",
+        help="For which analysis date to start the inference (default: yesterday)",
+    )
+
+    parser.add_argument(
+        "--time",
+        type=int,
+        default=12,
+        help="For which analysis time to start the inference (default: 12)",
+    )
+
+    parser.add_argument(
+        "--assets",
+        default=os.environ.get("AI_MODELS_ASSETS", "."),
+        help="Path to directory containing the weights and other assets",
+    )
+
+    parser.add_argument(
+        "--assets-sub-directory",
+        help="Load assets from a subdirectory of --assets based on the name of the model.",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--download-assets",
+        help="Download assets if they do not exists.",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--path",
+        default="out.grib",
+        help="Path where to write the output of the model",
+    )
+
+    parser.add_argument(
+        "--num-threads",
+        type=int,
+        default=1,
+        help="Number of threads. Only relevant for some models.",
+    )
+
+    parser.add_argument(
+        "--lead-time",
+        type=int,
+        default=240,
+        help="Length of forecast in hours.",
+    )
+
+    parser.add_argument(
+        "--model-version",
+        default="latest",
+        help="Model version",
+    )
+
+    if "--models" not in sys.argv:
+        parser.add_argument(
+            "model",
+            metavar="MODEL",
+            choices=available_models(),
+            help="The model to run",
+        )
+
+    args = parser.parse_args()
+
+    if args.models:
+        for p in sorted(available_models()):
+            print(p)
+        sys.exit(0)
+
+    if args.assets_sub_directory:
+        args.assets = os.path.join(args.assets, args.model)
+
+    logging.basicConfig(
+        level="DEBUG" if args.debug else "INFO",
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
+
+    if not check_gpus():
+        LOG.error("ai-models must be run on a node with GPUs")
+        sys.exit(1)
+
+    model = load_model(args.model, **vars(args))
+
+    try:
+        model.run()
+    except FileNotFoundError as e:
+        LOG.exception(e)
+        LOG.error(
+            "It is possible that some files requited by %s are missing.", args.model
+        )
+        LOG.error("Rerun the command as:")
+        LOG.error(
+            "   %s", shlex.join([sys.argv[0], "--download-assets"] + sys.argv[1:])
+        )
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ai_models/model.py
@@ -0,0 +1,156 @@
+# (C) Copyright 2023 European Centre for Medium-Range Weather Forecasts.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+import logging
+import os
+from functools import cached_property
+from importlib import import_module
+
+import climetlab as cml
+from multiurl import download
+
+LOG = logging.getLogger(__name__)
+
+
+class MarsInput:
+    def __init__(self, owner, **kwargs):
+        self.owner = owner
+
+    @cached_property
+    def fields_sfc(self):
+        LOG.info("Loading surface fields from MARS")
+        request = dict(
+            date=self.owner.date,
+            time=self.owner.time,
+            param=self.owner.param_sfc,
+            grid=self.owner.grid,
+            area=self.owner.area,
+            levtype="sfc",
+        )
+        return cml.load_source("mars", request)
+
+    @cached_property
+    def fields_pl(self):
+        LOG.info("Loading pressure fields from MARS")
+        param, level = self.owner.param_level_pl
+        request = dict(
+            date=self.owner.date,
+            time=self.owner.time,
+            param=param,
+            level=level,
+            grid=self.owner.grid,
+            area=self.owner.area,
+            levtype="pl",
+        )
+        return cml.load_source("mars", request)
+
+    @cached_property
+    def all_fields(self):
+        return self.fields_sfc + self.fields_pl
+
+
+class FileInput:
+    def __init__(self, owner, file, **kwargs):
+        self.file = file
+        self.owner = owner
+
+    @cached_property
+    def fields_sfc(self):
+        return cml.load_source("file", self.file).sel(levtype="sfc")
+
+    @cached_property
+    def fields_pl(self):
+        return cml.load_source("file", self.file).sel(levtype="pl")
+
+    @cached_property
+    def all_fields(self):
+        return cml.load_source("file", self.file)
+
+
+class FileOutput:
+    def __init__(self, owner, path, **kwargs):
+        LOG.info("Writting results to %s", path)
+        self.path = path
+        self.owner = owner
+        self.output = cml.new_grib_output(
+            path,
+            split_output=True,
+            class_="ml",
+            expver=owner.expver,
+            edition=2,
+        )
+
+    def write(self, *args, **kwargs):
+        self.output.write(*args, **kwargs)
+
+
+INPUTS = dict(mars=MarsInput, file=FileInput)
+
+OUTPUTS = dict(file=FileOutput)
+
+
+class Model:
+    assets_extra_dir = None
+
+    def __init__(self, input, output, download_assets, **kwargs):
+        self.input = INPUTS[input](self, **kwargs)
+        self.output = OUTPUTS[output](self, **kwargs)
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+        if self.assets_sub_directory:
+            if self.assets_extra_dir is not None:
+                self.assets += self.assets_extra_dir
+
+        LOG.debug("Asset directory is %s", self.assets)
+
+        try:
+            # For CliMetLab, when date=-1
+            self.date = int(self.date)
+        except ValueError:
+            pass
+
+        if download_assets:
+            self.download_assets(**kwargs)
+
+    @cached_property
+    def fields_pl(self):
+        return self.input.fields_pl
+
+    @cached_property
+    def fields_sfc(self):
+        return self.input.fields_sfc
+
+    @cached_property
+    def all_fields(self):
+        return self.input.all_fields
+
+    def write(self, *args, **kwargs):
+        self.output.write(*args, **kwargs)
+
+    def download_assets(self, **kwargs):
+        for file in self.download_files:
+            asset = os.path.realpath(os.path.join(self.assets, file))
+            if not os.path.exists(asset):
+                LOG.info("Downloading %s", asset)
+                download(self.download_url.format(file=file), asset + ".download")
+                os.rename(asset + ".download", asset)
+
+
+def available_models():
+    result = []
+    for path in os.listdir(os.path.join(os.path.dirname(__file__), "models")):
+        if path.startswith(".") or path.startswith("_"):
+            continue
+        name, _ = os.path.splitext(path)
+        result.append(name)
+    return result
+
+
+def load_model(name, **kwargs):
+    return import_module(f".{name}", package=__name__ + "s").model(**kwargs)
