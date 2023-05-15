fc8b04fbebfbc669c0b886f5981fe485694e98fd
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2023-05-15 07:40:06 +0100

    Missing tests

---
+++ tests/test_code.py
@@ -0,0 +1,10 @@
+# (C) Copyright 2023 European Centre for Medium-Range Weather Forecasts.
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+
+def test_code():
+    pass  # Empty for now
