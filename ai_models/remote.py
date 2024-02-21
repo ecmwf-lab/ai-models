130ba5b33483584c931e4edd41966b040395b008
Gert Mertes <hey@gertmertes.be>
2024-02-21 16:00:13 +0000

    Add remote file upload

--- ai_models/remote.py
+++ ai_models/remote.py
@@ -24,51 +24,69 @@ class RemoteClient:
         self.output_file = output_file
         self.input_file = input_file
         self._timeout = 10
+        self._status = self._last_status = None
 
     def run(self, cfg: dict, model_args: list):
         cfg.pop("remote_url", None)
         cfg.pop("remote_token", None)
         cfg["model_args"] = model_args
 
-        r = self._submit(cfg)
-        status = self._last_status
-        LOG.debug(r)
-        LOG.info("Job status: %s", self._last_status)
+        if self._upload(self.input_file) == "success":
+            self._submit(cfg)
 
         while not self._ready():
-            if status != self._last_status:
-                status = self._last_status
-                LOG.info("Job status: %s", status)
             time.sleep(5)
 
         download(urljoin(self.url, self._href), target=self.output_file)
 
-        LOG.info("Result written to %s", self.output_file)
+        LOG.debug("Result written to %s", self.output_file)
+
+    def _upload(self, file):
+        with open(file, "rb") as f:
+            LOG.info("Uploading input file to remote")
+            r = robust(requests.post, retry_after=self._timeout)(
+                urljoin(self.url, "upload"),
+                data=f,
+                auth=self.auth,
+                timeout=self._timeout,
+            ).json()
+
+        LOG.debug(r)
+        self._uid = r["id"]
+        self._href = r["href"]
+        self._status = r["status"].lower()
+
+        return self._status
 
     def _submit(self, data):
         r = robust(requests.post, retry_after=self._timeout)(
-            urljoin(self.url, "submit"),
+            urljoin(self.url, self._href),
             json=data,
             auth=self.auth,
             timeout=self._timeout,
-        )
-        res = r.json()
-        self._uid = res["id"]
-        self._href = res["href"]
-        self._last_status = res["status"]
-        return res
+        ).json()
 
-    def _status(self):
+        LOG.debug(r)
+        self._uid = r["id"]
+        self._href = r["href"]
+        self._status = r["status"].lower()
+
+        return self._status
+
+    def _poll(self):
         r = robust(requests.get, retry_after=self._timeout)(
             urljoin(self.url, self._href), auth=self.auth, timeout=self._timeout
-        )
+        ).json()
 
-        res = r.json()
-        LOG.debug(res)
-        self._href = res["href"]
-        self._last_status = res["status"]
+        LOG.debug(r)
+        self._href = r["href"]
+        self._status = r["status"].lower()
 
-        return self._last_status
+        if self._last_status != self._status:
+            LOG.info("Job status: %s", self._status)
+            self._last_status = self._status
+
+        return self._status
 
     def _ready(self):
-        return self._status().lower() == "ready"
+        return self._poll() == "ready"
